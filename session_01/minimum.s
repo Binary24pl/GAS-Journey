912b4da428c8c3a94d3ff0cdcf957f3d0d9bd0f7
Binary24pl <scpbinary@gmail.com>
2026-07-02 20:15:34 +0200

    starting new session, and I added comments to minimum.s

--- session_01/minimum.s
+++ session_01/minimum.s
@@ -10,22 +10,23 @@ data_items:
 
 data_items_end:
 .long 0
+# I utulize the fact that Im using a memory heap, so in short, Im using adress of data_items_end later on
 
 .section .text
 .globl _start
 
 _start:
-movl $data_items, %esi
-movl $data_items_end, %edi
-movl (%esi), %eax
-movl %eax, %ebx
+movl $data_items, %esi #Labels are adressies, so I move an adress to the esi
+movl $data_items_end, %edi #And so on
+movl (%esi), %eax #(%esi) is like an * in C pointers, but we do need an actual adress
+movl %eax, %ebx #same as in maximum.s, we treat ebx as a final answer
 
 start_loop:
-addl $4, %esi
+addl $4, %esi #unlike the labelname(base,offset,word), we do calculation of a memory position directly
 cmpl %edi, %esi
-je loop_exit
+je loop_exit #and then we check if we reach data_items_end
 
-movl (%esi), %eax
+movl (%esi), %eax #and a rest is similar to maximum.s
 cmpl %ebx, %eax
 jge start_loop
 
